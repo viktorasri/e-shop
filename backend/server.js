23956bde86a56bb778eb037232f4694f8795b21b
Viktoras Rimkus <viktorasrimkus@Viktorass-MacBook-Pro.local>
2020-10-23 18:00:19 +0300

    enable ES Modules in node.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const products = require('./data/products.js');
-const dotenv = require('dotenv');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
 
 const app = express();
 dotenv.config();
