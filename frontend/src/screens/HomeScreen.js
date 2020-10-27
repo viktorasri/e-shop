75fbb6d9513add20f197caa275966ca12a670884
Viktoras Rimkus <viktorasrimkus@Viktorass-MacBook-Pro.local>
2020-10-27 22:21:20 +0200

    add products list action, reducer for homescreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,39 +1,42 @@
-import React,  { useState, useEffect} from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Col, Row } from 'react-bootstrap';
-import Axios from 'axios'
 
+import { listProducts } from '../actions/productActions';
 import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
 
-    useEffect(()=>{
-        const fetchProducts = async () => {
-            try {
-                const {data} = await Axios.get('/api/products');
-                setProducts(data);
-            } catch (error) {
-                console.log(error)
-            }
-        }
+  const productList = useSelector((state) => state.productList);
+  const { loading, products, error } = productList;
 
-        fetchProducts();
-    },[]);
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
-    return (
-        <>
-            <h1>Latest products</h1>
-            <Row>
-                {products.map(product => {
-                    return (
-                        <Col key={product._id} sm={12} md={6} lg={4}>
-                            <Product  product={product} />
-                        </Col>
-                    )
-                })}
-            </Row>    
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Latest products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => {
+            return (
+              <Col key={product._id} sm={12} md={6} lg={4}>
+                <Product product={product} />
+              </Col>
+            );
+          })}
+        </Row>
+      )}
+    </>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
