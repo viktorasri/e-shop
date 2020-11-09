966c796bc8db3ec6afadbd2557b8431dbf7dec57
Viktoras Rimkus <viktorasrimkus@Viktorass-MacBook-Pro.local>
2020-11-09 14:51:52 +0200

    add create & update product endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -38,4 +38,48 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProductById, getProducts, deleteProduct };
+//  @desc   Create product
+//  @route  POST /api/products
+//  @access Private / Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const { name, image, description, brand, category, price, countInStock } = req.body;
+  const product = new Product({
+    user: req.user._id,
+    name: name || 'Sample product',
+    image: image || '/images/sample.jpg',
+    description: description || 'Sample description',
+    brand: brand || 'Sampe brand',
+    category: category || 'Sample category',
+    price: price || 0,
+    countInStock: countInStock || 0,
+  });
+
+  const savedProduct = await product.save();
+  res.status(201).json(savedProduct);
+});
+
+//  @desc   Update product
+//  @route  PUT /api/products/:id
+//  @access Private / Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const { name, image, description, brand, category, price, countInStock } = req.body;
+    product.name = name || product.name;
+    product.image = image || product.image;
+    product.description = description || product.description;
+    product.brand = brand || product.brand;
+    product.category = category || product.category;
+    product.price = price || product.price;
+    product.countInStock = countInStock || product.countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+export { getProductById, getProducts, deleteProduct, createProduct, updateProduct };
